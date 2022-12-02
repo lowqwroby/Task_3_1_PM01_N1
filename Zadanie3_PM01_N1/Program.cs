1987e3a6d928144ed09ccb21b22b9c967ae0b53b
lowqwroby <eltiobvs@gmail.com>
2022-12-02 22:19:29 +0300

    Добавьте файлы проекта.

---
+++ Zadanie3_PM01_N1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Variant10
+{
+	class Program
+	{
+		static double fx(double number)
+		{
+			return (number % 5 == 0) ? number / 5 : number + 1;
+		}
+
+		static void Main()
+		{
+			try
+			{
+				Console.Write("Введите любое число^ ");
+				double number = double.Parse(Console.ReadLine());
+				double res = fx(number);
+				Console.WriteLine(res);
+			}
+			catch
+			{
+				Console.WriteLine("Введены некорректные данные.");
+			}
+			
+		}
+	}
+}
